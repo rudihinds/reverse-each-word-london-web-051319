56f49def784abfb1cad5c90c5585f708baf86d5b
rudi hinds <rudihinds@gmail.com>
2019-05-02 16:10:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,10 +3,8 @@ newarr = string.split(" ")
 end
 
 
-reversed = reverse_each_word("Hello there, and how are you?").collect do |w|
+reverse_each_word("Hello there, and how are you?").collect do |w|
   w.reverse
-  
-reversed.join
 end
 
 
