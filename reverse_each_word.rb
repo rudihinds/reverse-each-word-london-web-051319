e8ececacfc89d7358580ffb6fef759f7684bbaad
rudi hinds <rudihinds@gmail.com>
2019-05-02 16:26:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,8 +7,9 @@ def reverse_each_word(array)
 array.collect do |w|
   w.reverse
 end
-reverse_each_word(array).join
+
 end
+reverse_each_word(array).join
 
 
 # reverse_each_word(["Hello", "there,", "and", "how", "are", "you?"])
