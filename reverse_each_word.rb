1288ac32e550016c6a5c2ba0c9f89887989abb61
rudi hinds <rudihinds@gmail.com>
2019-05-02 16:35:34 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+def reverse_setup(string)
+string.split(" ")
+end
+
+
+def reverse_each_word(string)
+newArr = string.split(" ").collect do |w|
+  w.reverse
+end
+  newArr.join(" ")
+end
+
+
+# reverse_each_word("Hello" "there,", "and", "how", "are", "you?")
